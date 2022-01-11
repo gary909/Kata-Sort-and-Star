1f708af9ea5d0a3eea1ce2a8633039a09faa4f83
gary <gary.white@lodgit.com>
2022-01-11 13:07:29 +0100

    template

---
+++ index.js
@@ -0,0 +1,6 @@
+function twoSort(s) {
+
+}
+
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"])); // Return : 'b***i***t***c***o***i***n'
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"])); // Return : 'a***r***e'
