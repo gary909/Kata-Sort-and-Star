9667fab01f7be5f15473483c2c781f57870343bd
gary <gary.white@lodgit.com>
2022-01-11 16:39:30 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function twoSort(s) {
+// function twoSort(s) {
 
+//     // s.sort(); sorts alphabet, not ascii
+//     let myAns = s.map(function(newThing) {
+//         return newThing.charAt(0);
+//       });
+//       console.log(myAns.join(''));
+//     //   console.log(myAns.join('***'));
+// }
+
+function twoSort(s) {
+    const sorted = s.sort(); // the default sort function works
+    return sorted[0] // take first element
+      .split("") // split string into chars
+      .join("***"); // join chars with ***
 }
 
 console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"])); // Return : 'b***i***t***c***o***i***n'
